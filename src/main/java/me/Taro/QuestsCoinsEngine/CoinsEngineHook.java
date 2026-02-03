ad045a0ca2942aa99f298c7752ab91a589de5d85
Lonedraw <100482867+Lonedraw@users.noreply.github.com>
2026-02-04 00:41:14 +0100

    Update CoinsEngineHook.java

--- src/main/java/me/Taro/QuestsCoinsEngine/CoinsEngineHook.java
+++ src/main/java/me/Taro/QuestsCoinsEngine/CoinsEngineHook.java
@@ -1,31 +1,31 @@
 package me.taro.questscoinsengine;
 
+import me.nulli.coinsengine.api.CoinsEngineAPI;
+import me.nulli.coinsengine.api.currency.Currency;
+import me.nulli.coinsengine.api.user.User;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.entity.Player;
-import su.nightexpress.nightcore.api.NightCoreAPI;
-import su.nightexpress.nightcore.api.coins.CoinsManager;
 
 import java.util.UUID;
 
 public class CoinsEngineHook {
 
     private final Plugin plugin;
-    private final CoinsManager coinsManager;
 
     public CoinsEngineHook(Plugin plugin) {
         this.plugin = plugin;
-        this.coinsManager = NightCoreAPI.getInstance().getCoinsManager();
     }
 
-    public void giveCoins(UUID uuid, double amount) {
-        // NightCore CoinsManager automatically uses the default currency
-        boolean success = coinsManager.addCoins(uuid, amount);
-        if (!success) {
-            plugin.getLogger().warning("Failed to add coins to player: " + uuid);
+    public void giveCoins(UUID uuid, String currencyId, double amount) {
+        Currency currency = CoinsEngineAPI.getCurrencyManager().getCurrency(currencyId);
+
+        if (currency == null) {
+            plugin.getLogger().warning(
+                    "Currency '" + currencyId + "' does not exist."
+            );
+            return;
         }
-    }
 
-    public void giveCoins(Player player, double amount) {
-        giveCoins(player.getUniqueId(), amount);
+        User user = CoinsEngineAPI.getUserManager().getOrCreateUser(uuid);
+        user.addCoins(currency, amount);
     }
 }
