f80542fb283565248b62e4450d7dcf5b0e4ea47e
Lonedraw <100482867+Lonedraw@users.noreply.github.com>
2026-02-04 02:53:06 +0100

    Update CoinsEngineHook.java

--- src/main/java/me/Taro/QuestsCoinsEngine/CoinsEngineHook.java
+++ src/main/java/me/Taro/QuestsCoinsEngine/CoinsEngineHook.java
@@ -16,7 +16,7 @@ public class CoinsEngineHook {
     }
 
     public void giveCoins(UUID uuid, String currencyId, double amount) {
-        Currency currency = CoinsEngineAPI.getCurrencyManager().getCurrencies();
+        Currency currency = CoinsEngineAPI.getCurrencyManager().getCurrencies("default");
 
         if (currency == null) {
             plugin.getLogger().warning(
