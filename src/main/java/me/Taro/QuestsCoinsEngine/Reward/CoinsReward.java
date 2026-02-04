404e86183a2351138c98b83b2378ef04f7b56960
Lonedraw <100482867+Lonedraw@users.noreply.github.com>
2026-02-04 01:11:47 +0100

    Update CoinsReward.java

--- src/main/java/me/Taro/QuestsCoinsEngine/Reward/CoinsReward.java
+++ src/main/java/me/Taro/QuestsCoinsEngine/Reward/CoinsReward.java
@@ -1,7 +1,7 @@
 package me.Taro.QuestsCoinsEngine.Reward;
 
 import org.bukkit.entity.Player;
-import su.nightexpress.coinsengine.CoinsEngine; // NightExpress API
+import su.nightexpress.coinsengine.CoinsEnginePlugin; // NightExpress API
 import su.nightexpress.coinsengine.api.CurrencyManager;
 
 public class CoinsReward {
